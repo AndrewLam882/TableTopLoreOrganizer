1ce22c43e7ae83c7ea2f8d1b48291058ba20d497
Andrew Lam <andrewl@ensemble.com>
2023-06-09 12:28:19 -0700

    remove unneeded accordion

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,8 +1,9 @@
-import { Box, Stack, VStack, useDisclosure } from '@chakra-ui/react'
+import { Box, Divider, Stack, VStack, useDisclosure, Text } from '@chakra-ui/react'
 import { BiImport, BiExport } from 'react-icons/bi'
+import { BsPersonFillAdd } from 'react-icons/bs'
+import { MdAddLocationAlt } from 'react-icons/md'
 import ButtonRoute from '../../types/buttonRoute'
 import NavButton from './navButton'
-import AddContentNav from './addContentNav'
 
 import LocalizedStrings from '../../localization'
 import JsonImportModal from '../modals/jsonImportModal'
@@ -28,13 +29,18 @@ const NavBar = ({ routes }: NavBarPropsType) => {
               label={label}
             />
           ))}
-
-          <AddContentNav />
         </VStack>
 
-        <VStack p='16px 0' borderTop='1px solid white' spacing='16px'>
+        <VStack spacing='16px'>
+          <Divider />
+          <NavButton label={strings.add.character} leftIcon={<BsPersonFillAdd />} />
+          <NavButton label={strings.add.location} leftIcon={<MdAddLocationAlt />} />
+          <Divider />
           <NavButton label={strings.import} leftIcon={<BiImport />} onClick={onImportOpen} />
           <NavButton label={strings.export} leftIcon={<BiExport />} />
+          <Text color='gray.600' fontSize='10px'>
+            Created by Andrew Lam
+          </Text>
         </VStack>
       </Stack>
 
